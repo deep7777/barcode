4bda08350622b01ac04f9e0b3f4eb6fd10f2b95a
Deepak Yadav <deepakyadav@Deepaks-MacBook-Pro.local>
2016-02-16 16:32:30 +0530

    add barcode

--- index.php
+++ index.php
@@ -1,28 +1,27 @@
-<?php
-?>
 <html>
-<div id="barcodes" class="bc" style="display:none;">
-<div>
-<button id="btn_print1" onclick="window.print();">Print</button>
-</div>
-<img class="bcodes" id="barcode0">
-<img class="bcodes" id="barcode1">
-<img class="bcodes" id="barcode2">
-<img class="bcodes" id="barcode3">
-<img class="bcodes" id="barcode4">
-<img class="bcodes" id="barcode5">
-<img class="bcodes" id="barcode6">
-<img class="bcodes" id="barcode7">
-<img class="bcodes" id="barcode8">
-<img class="bcodes" id="barcode9">
-<img class="bcodes" id="barcode10">
-<img class="bcodes" id="barcode11">
-<img class="bcodes" id="barcode12">
-<img class="bcodes" id="barcode13">
-<img class="bcodes" id="barcode14">
+<div id="barcodes"  style="display:none;">
+	<div>
+	<button id="btn_print1" onclick="window.print();">Print</button>
+	</div>
+	<img class="bcodes" id="barcode0">
+	<img class="bcodes" id="barcode1">
+	<img class="bcodes" id="barcode2">
+	<img class="bcodes" id="barcode3">
+	<img class="bcodes" id="barcode4">
+	<img class="bcodes" id="barcode5">
+	<img class="bcodes" id="barcode6">
+	<img class="bcodes" id="barcode7">
+	<img class="bcodes" id="barcode8">
+	<img class="bcodes" id="barcode9">
+	<img class="bcodes" id="barcode10">
+	<img class="bcodes" id="barcode11">
+	<img class="bcodes" id="barcode12">
+	<img class="bcodes" id="barcode13">
+	<img class="bcodes" id="barcode14">
 </div>
 <input id="barcode" type="text" value="" maxlength="9" placeholder="Scan Barcode" style="text-transform: uppercase;">
 <button id="btn">code</button>
+
 <script src="/bower_components/JsBarcode/JsBarcode.all.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.0/jquery.js"></script>
 
@@ -51,7 +50,6 @@ function jsbarcodes(barcode_val){
 	JsBarcode("#barcode14",barcode_val,{format:"CODE39",displayValue:true,width:2,height:25});
 	myWindow = window.open("", "MsgWindow", "width=400, height=400");
 	myWindow.document.write($("#barcodes").html());	
-	$(".bc").hide();
 }
 
 
